270c44b9310394b0a1a8d55f7142a49e42bfdf09
AF <ailtonferraz56@gmail.com>
2023-02-14 01:36:35 +0000

    account

--- src/entities/BusinessAccount.java
+++ src/entities/BusinessAccount.java
@@ -1,5 +1,32 @@
 package entities;
 
-public class BusinessAccount {
+public class BusinessAccount extends Account {
+	
+	private Double loanLimit;
+	
+	
+	public BusinessAccount() {
+		super();
+	}
+	
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+	
+	public Double getLoandLimit() {
+		return loanLimit;
+	}
+
+	public void setLoandLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+
+	public void loan(double amount) {
+		if(amount <= loanLimit) {
+			balance += amount - 10;
+		}
+	}
+	
 
 }
